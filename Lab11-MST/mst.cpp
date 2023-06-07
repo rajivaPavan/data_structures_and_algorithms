3a3f144bf13cb0e98110257b297db8f84b8d89ab
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-07 22:52:19 +0530

    finished prims algorithm

--- Lab11-MST/mst.cpp
+++ Lab11-MST/mst.cpp
@@ -3,84 +3,87 @@
 #include <queue>
 using namespace std;
 #define N 6
+#define PVW pair<int,int> //pair of vertex and weight of edge connected to it
 
 int** initializeGraph();
-void printGraph(int** graph, int n);
-int** primsMST(int** graph, int n_vertices);
-pair<int, int> minSafeEdge(vector<pair<int, int>> edges, bool mstSet[]);
+void primsMST(int graph[N][N], int n_vertices);
+void printMST(int parent[], int graph[6][6], int root);
 
 int main(){
-    int** graph = initializeGraph();
-    int** mst = primsMST(graph, N);
-    printGraph(mst, N);
+    int graph[N][N] = {
+            {0,3,0,0,0,1},
+            {3,0,2,1,10,0},
+            {0,2,0,3,0,5},
+            {0,1,3,0,5,0},
+            {0,10,0,5,0,4},
+            {1,0,5,0,4,0}
+    };
+    primsMST(graph, N);
     return 0;
 }
 
+class CompareEdges{
+    public:
+        bool operator()(PVW below, PVW above){
+            // return true if the node below has a lower weighted edge
+            return(below.second > above.second);
+        }
+};
+
 /// @param graph a connect and undirected graph
 /// @return a minimum spanning tree
-int** primsMST(int** graph, const int n_vertices){
+void primsMST(int graph[N][N], const int n_vertices){
     // set of vertices in MST;
     bool mstVertices[n_vertices];
+
+    // initialize all vertices as not in the mst
+    for(int i = 0; i < n_vertices; i++){
+        mstVertices[i] = false;
+    }
    
     // parent of vertices in sub-trees
     int parent[n_vertices];
 
-    // start with 0 as the root node
-    parent[0] = 0; 
-    mstVertices[0] = true;
+    // priority queue of vertices and edge weights
+    // Used to select the minimum safe edge
+    priority_queue<PVW, vector<PVW>, CompareEdges> pq;
+
+    // root with 0 as the root node
+    int root = 0;
+    parent[root] = -1;
 
     // edges from parent vertices to fringe vertices
-    vector<pair<int, int>> edges;
+    pq.push({root, 0});
 
     // go through all fringe vertices
     for(int i = 0; i < n_vertices - 1; i++){
         // get minimum edge connected to fringe vertex from a vertex in the tree
-        pair<int, int> edge = minSafeEdge(edges, mstVertices);
-        
-        // add first as the parent of the second
-        parent[edge.second] = edge.first;
+        PVW  minSafeEdge = pq.top();
+        pq.pop();
 
         // add second to the mst vertices set
-        mstVertices[edge.second] = true;
+        int u = minSafeEdge.first;
+        mstVertices[u] = true;
+
+        // add edges of newly added vertex, which are not already in the mst
+        for(int v = 0 ; v < n_vertices; v++){
+            if(graph[u][v] && !mstVertices[v]){
+                parent[v] = u;
+                pq.push({v, graph[u][v]});
+            }
+        }
     }
-    
-}
 
-/// @brief Gets minimum edge connected to fringe vertex from a vertex in the tree
-/// @param edges 
-/// @param mstSet array of vertices in the mst
-/// @return edge with minimum weight to be added to the mst. 
-/// pair : first - mst vertex, second - fringe vertex to be added
-pair<int, int> minSafeEdge(vector<pair<int, int>> edges, bool mstSet){
+    printMST(parent, graph, root);
 
 }
 
-/// @brief prints the edges of the given graph
-/// @param graph adjacency matrix
-/// @param n number of vertices
-void printGraph(int** graph, int n){
-    cout << "The graph is a follows: " << endl;
-    
-}
-
-/// @return a hard coded graph adjacency matrix of 6 x 6
-int** initializeGraph(){
-    int graph[N][N] = {
-        {0,3,0,0,0,1},
-        {3,0,2,1,10,0},
-        {0,2,0,3,0,5},
-        {0,1,3,0,5,0},
-        {0,10,0,5,0,4},
-        {1,0,5,0,4,0}
-    };
-    
-    int** G = new int*[N];
-    for(int i = 0; i < N; i++){
-        G[i] = new int[N];
-        for(int j = 0; j < N; j++)
-            G[i][j] = graph[i][j];
-    }
-    
-    return G;
+void printMST(int parent[], int graph[6][6], int root)
+{
+    cout << "Edge \tWeight\n";
+    for (int i = 0; i < N; i++)
+        if(i != root)
+        cout << parent[i] << " - " << i << " \t"
+             << graph[i][parent[i]] << " \n";
 }
 
